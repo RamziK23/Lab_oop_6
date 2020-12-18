7b610b8b6c93199f50de88d424f5740d9ac1383f
RamziK23 <ramzesdavletbaev23@gmail.com>
2020-12-18 11:30:51 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace lab_oop_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paint_box = new System.Windows.Forms.Panel();
+            this.button_show = new System.Windows.Forms.Button();
+            this.button_deletestorage = new System.Windows.Forms.Button();
+            this.button_del__item_storage = new System.Windows.Forms.Button();
+            this.button_clear_paintbox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // paint_box
+            // 
+            this.paint_box.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.paint_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paint_box.Location = new System.Drawing.Point(12, 13);
+            this.paint_box.Name = "paint_box";
+            this.paint_box.Size = new System.Drawing.Size(675, 426);
+            this.paint_box.TabIndex = 0;
+            this.paint_box.MouseClick += new System.Windows.Forms.MouseEventHandler(this.paint_box_MouseClick);
+            // 
+            // button_show
+            // 
+            this.button_show.Location = new System.Drawing.Point(693, 12);
+            this.button_show.Name = "button_show";
+            this.button_show.Size = new System.Drawing.Size(95, 37);
+            this.button_show.TabIndex = 1;
+            this.button_show.Text = "button_show";
+            this.button_show.UseVisualStyleBackColor = true;
+            this.button_show.Click += new System.EventHandler(this.button_show_Click);
+            this.button_show.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button_deletestorage
+            // 
+            this.button_deletestorage.Location = new System.Drawing.Point(693, 55);
+            this.button_deletestorage.Name = "button_deletestorage";
+            this.button_deletestorage.Size = new System.Drawing.Size(95, 36);
+            this.button_deletestorage.TabIndex = 2;
+            this.button_deletestorage.Text = "button_deletestorage";
+            this.button_deletestorage.UseVisualStyleBackColor = true;
+            this.button_deletestorage.Click += new System.EventHandler(this.button_deletestorage_Click);
+            this.button_deletestorage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button_del__item_storage
+            // 
+            this.button_del__item_storage.Location = new System.Drawing.Point(693, 97);
+            this.button_del__item_storage.Name = "button_del__item_storage";
+            this.button_del__item_storage.Size = new System.Drawing.Size(95, 37);
+            this.button_del__item_storage.TabIndex = 3;
+            this.button_del__item_storage.Text = "button_del__item_storage";
+            this.button_del__item_storage.UseVisualStyleBackColor = true;
+            this.button_del__item_storage.Click += new System.EventHandler(this.button_del__item_storage_Click);
+            this.button_del__item_storage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button_clear_paintbox
+            // 
+            this.button_clear_paintbox.Location = new System.Drawing.Point(693, 140);
+            this.button_clear_paintbox.Name = "button_clear_paintbox";
+            this.button_clear_paintbox.Size = new System.Drawing.Size(95, 36);
+            this.button_clear_paintbox.TabIndex = 4;
+            this.button_clear_paintbox.Text = "button_clear_paintbox";
+            this.button_clear_paintbox.UseVisualStyleBackColor = true;
+            this.button_clear_paintbox.Click += new System.EventHandler(this.button_clear_paintbox_Click);
+            this.button_clear_paintbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_clear_paintbox);
+            this.Controls.Add(this.button_del__item_storage);
+            this.Controls.Add(this.button_deletestorage);
+            this.Controls.Add(this.button_show);
+            this.Controls.Add(this.paint_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel paint_box;
+        private System.Windows.Forms.Button button_show;
+        private System.Windows.Forms.Button button_deletestorage;
+        private System.Windows.Forms.Button button_del__item_storage;
+        private System.Windows.Forms.Button button_clear_paintbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_oop_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int p = 0;
+        static int k = 5;
+        Storage storag = new Storage(k);
+        static int index = 0;
+        int indexin = 0;
+
+        class Circle
+        {
+            public int x, y;
+            public int rad = 15;
+            public Color color = Color.Navy;
+            public bool is_drawed = true;
+            public Circle()
+            {
+                x = 0;
+                y = 0;
+            }
+            public Circle(int x, int y)
+            {
+                this.x = x - rad;
+                this.y = y - rad;
+            }
+
+            ~Circle() { }
+        }
+
+        class Storage
+        {
+            public Circle[] objects;
+            public Storage(int count)
+            {
+                objects = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+            public void initialisat(int count)
+            {//выделяем место
+                objects = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                    objects[i] = null;
+            }
+            public void add_object(int ind, ref Circle object1, int count, ref int indexin)
+            {//добавляет объект
+                while (objects[ind] != null)
+                {//если ячейка занята, ищем новое место
+                    ind = (ind + 1) % count;
+                }
+                objects[ind] = object1;
+                indexin = ind;
+            }
+            public void delete_object(int ind)
+            {//удаляет объект из хранилища
+                objects[ind] = null;
+                index--;
+            }
+            public bool check_empty(int index)
+            {//занято ли место
+                if (objects[index] == null)
+                    return true;
+                else
+                    return false;
+            }
+            public int occupied(int size)
+            {//кол-во занятых мест
+                int count_occupied = 0;
+                for (int i = 0; i < size; ++i)
+                    if (!check_empty(i))
+                        ++count_occupied;
+                return count_occupied;
+            }
+            public void doubleSize(ref int size)
+            {//расширение хранилища
+                Storage storage1 = new Storage(size + 10);
+                for (int i = 0; i < size; ++i)
+                    storage1.objects[i] = objects[i];
+                for (int i = size; i < (size + 10) - 1; ++i)
+                {
+                    storage1.objects[i] = null;
+                }
+                size = size + 10;
+                initialisat(size);
+                for (int i = 0; i < size; ++i)
+                    objects[i] = storage1.objects[i];
+            }
+
+
+
+            ~Storage() { }
+        };
+
+        private void paint_circle(Color name, ref Storage stg, int index)
+        {//рисуем круг на панели
+            Pen pen = new Pen(name, 3);
+
+            if (!storag.check_empty(index))
+            {
+                if (storag.objects[index].is_drawed == true)
+                {
+                    paint_box.CreateGraphics().DrawEllipse(
+                    pen,
+                    stg.objects[index].x,
+                    stg.objects[index].y,
+                    stg.objects[index].rad * 2,
+                    stg.objects[index].rad * 2);
+                    stg.objects[index].color = name;
+                }
+            }
+        }
+
+        private void button_show_Click(object sender, EventArgs e)
+        {
+            paint_box.Refresh();
+            if (storag.occupied(k) != 0)
+            {
+                for (int i = 0; i < k; ++i)
+                {
+                    if (!storag.check_empty(i))
+                    {
+                        storag.objects[i].is_drawed = true;
+                    }
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+        }
+
+        private void button_deletestorage_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < k; ++i)
+            {
+                storag.objects[i] = null;
+            }
+            index = 0;
+        }
+
+        private void button_del__item_storage_Click(object sender, EventArgs e)
+        {
+            remove_selected_circle(ref storag);
+            paint_box.Refresh();
+            if (storag.occupied(k) != 0)
+            {
+                for (int i = 0; i < k; ++i)
+                {
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+        }
+
+        private void button_clear_paintbox_Click(object sender, EventArgs e)
+        {
+            paint_box.Invalidate();
+            paint_box.Refresh();
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {
+                    storag.objects[i].is_drawed = false;
+                }
+            }
+        }
+
+        private void remove_selected_circle(ref Storage stg)
+        {//удаляет выделенные элементы
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {
+                    if (storag.objects[i].color == Color.Red)
+                    {
+                        storag.delete_object(i);
+                    }
+                }
+            }
+        }
+
+        private void paint_box_MouseClick(object sender, MouseEventArgs e)
+        {
+            //проверка на наличие круга на данных координатах
+            int c = check_circle(ref storag, k, e.X - 15, e.Y - 15);
+            if (index == k)
+                storag.doubleSize(ref k);
+            if (c != -1)
+            {//круг уже есть
+                if (Control.ModifierKeys == Keys.Control)
+                {//если нажат, выделяем несколько объектов
+                    if (p == 0)
+                    {
+                        paint_circle(Color.Navy, ref storag, indexin);
+                        p = 1;
+                    }
+                    paint_circle(Color.Red, ref storag, c);
+                }
+                else
+                {//иначе только один объект
+                    remove_selection_circle(ref storag);
+
+                    paint_circle(Color.Red, ref storag, c);
+                }
+                return;
+            }
+            else
+            {//круга нет
+                Circle krug = new Circle(e.X, e.Y);
+
+                storag.add_object(index, ref krug, k, ref indexin);
+
+                remove_selection_circle(ref storag);
+
+                paint_circle(Color.Red, ref storag, indexin);
+                ++index;
+
+            }
+            p = 0;
+        }
+
+        private int check_circle(ref Storage stg, int size, int x, int y)
+        {//проверка на наличие круга с координатами в хранилище
+            if (stg.occupied(size) != 0)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    if (!stg.check_empty(i))
+                    {
+                        int x1 = stg.objects[i].x - 15;
+                        int x2 = stg.objects[i].x + 15;
+                        int y1 = stg.objects[i].y - 15;
+                        int y2 = stg.objects[i].y + 15;
+                        //если круг есть, возвращает индекс круга
+                        if ((x1 <= x && x <= x2) && (y1 <= y && y <= y2))
+                            return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private void remove_selection_circle(ref Storage stg)
+        {//снимает выделение
+            for (int i = 0; i < k; ++i)
+            {
+                if (!storag.check_empty(i))
+                {
+                    paint_circle(Color.Navy, ref storag, i);
+                }
+            }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {//кнопка Delete
+            if (e.KeyValue == (char)Keys.Delete)
+            {
+                remove_selected_circle(ref storag);
+                paint_box.Refresh();
+                if (storag.occupied(k) != 0)
+                {
+                    for (int i = 0; i < k; ++i)
+                    {
+                        paint_circle(Color.Navy, ref storag, i);
+                    }
+                }
+            }
+        }
+    }
+}
